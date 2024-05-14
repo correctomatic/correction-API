8355285e4bc4b349f9f341c84294ce365a9a3ba4
Alvaro Maceda <alvaro@alvaromaceda.es>
2024-05-14 13:38:10 +0200

    Sets name for the jobs

--- src/lib/bullmq.js
+++ src/lib/bullmq.js
@@ -5,9 +5,10 @@ import logger from '../logger.js'
 const { QUEUE_NAME, QUEUE_CONFIG } = env.bullMQ
 const queue = new Queue(QUEUE_NAME,QUEUE_CONFIG)
 
-async function putInPendingQueue(message) {
-  logger.info('Putting message in pending queue')
-  return queue.add('pending correction', message)
+async function putInPendingQueue(job) {
+  const name = `Correction${job.work_id ? ' ' + job.work_id : ''}`
+  logger.info(`Putting job in pending queue. Work id: ${job.work_id}`)
+  return queue.add(name, job)
 }
 
 export {
