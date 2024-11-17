c76fe9c840a2cea9191507d03ae3028eb22bbec8
Alvaro Maceda <alvaro@alvaromaceda.es>
2024-11-17 13:19:56 +0100

    Checks if user exists before creating user in utility script

--- src/create_user.js
+++ src/create_user.js
@@ -1,18 +1,38 @@
 const initDB = require('./db/index.js')
+const db = initDB()
+
+async function getUser(user) {
+  return await db.models.User.findOne({ where: { user } })
+}
 
 async function createUser(user, password, roles) {
-  initDB()
-  const db = initDB()
-
-  await db.sequelize.authenticate()
-  console.info('Database connection has been established successfully.')
-  const userInstance = await db.models.User.create({
-    user: user,
+  await db.models.User.create({
+    user,
     roles: Array.from(roles),
-    password: password
+    password,
   })
+  console.info(`User '${user}' has been created with roles: ${roles.join(',')}`)
+}
 
-  await db.sequelize.close()
+async function performCreation(user, password, roles) {
+  try {
+    await db.sequelize.authenticate()
+    console.info('Database connection has been established successfully.')
+
+    const existingUser = await getUser(user)
+    if (existingUser) {
+      console.info(`User '${user}' already exists with roles: ${existingUser.roles.join(',')}`)
+      return
+    }
+
+    await createUser(user, password, roles)
+  } catch (error) {
+    console.error('An error occurred:', error)
+    process.exit(1)
+  } finally {
+    await db.sequelize.close()
+    console.info('Database connection closed.')
+  }
 }
 
 const args = process.argv.slice(2) // Skip the first two arguments (node and script path)
@@ -24,5 +44,5 @@ if (args.length < 2) {
 const [user, password, rolesArg] = args
 const roles = rolesArg?.split(',').map(role => role.trim())
 
-console.info(`Creating user ${user} with roles: ${roles?.join(',') || 'none'}`)
-createUser(user, password, roles)
+console.info(`Processing user ${user} with roles: ${roles?.join(',') || 'none'}`)
+performCreation(user, password, roles)
