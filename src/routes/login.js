49708f602dd004d25f064957afc314de7538d91c
Alvaro Maceda <alvaro@alvaromaceda.es>
2024-11-17 14:06:27 +0100

    Solves error in invalid login response

--- src/routes/login.js
+++ src/routes/login.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken')
 const env = require('../config/env')
 
 const { LOGIN_SCHEMA } = require('../schemas/login_schema')
+const errorResponse = require('../lib/requests').errorResponse
 
 async function routes(fastify, _options) {
 
@@ -17,7 +18,7 @@ async function routes(fastify, _options) {
       const userInstance = await User.findOne({ where: { user } })
 
       if (!userInstance || ! await userInstance.validatePassword(password)) {
-        return reply.status(401).send({ message: 'Invalid credentials' })
+        return reply.status(401).send(errorResponse('Invalid credentials'))
       }
 
       const token = jwt.sign({ user: userInstance.user }, env.jwt.secretKey, { expiresIn: env.jwt.expiration })
